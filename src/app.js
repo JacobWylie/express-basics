63d2578640d3eedf0fa0de2a2c1e8f150879e098
JacobWylie <jacobwylie@gmail.com>
2017-03-12 15:27:25 +0100

    added home route

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+	response.send('I Love Treehouse!');
+});
+
+app.listen(3000);
